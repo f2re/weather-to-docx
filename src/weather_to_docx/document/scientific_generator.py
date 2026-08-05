9fcfd53410b5f55fc5a7e03bc8b145439ab63077
F2re <f2re@users.noreply.github.com>
2026-08-05 09:22:00 +0300

    Подключён русифицированный генератор метеограмм

--- src/weather_to_docx/document/scientific_generator.py
+++ src/weather_to_docx/document/scientific_generator.py
@@ -1,5 +1,14 @@
-"""Совместимая точка импорта генератора DOCX с метеограммами."""
+"""Совместимая точка импорта русифицированного генератора DOCX."""
 
-from weather_to_docx.document.meteogram_document import ScientificDocumentGenerator
+from weather_to_docx.document import meteogram_document as _meteogram_document
+from weather_to_docx.document.russian_labels import apply_russian_display_names
+from weather_to_docx.plotting.russian_meteogram import RussianMeteogramRenderer
+
+apply_russian_display_names()
+_meteogram_document.MeteogramRenderer = RussianMeteogramRenderer
+
+from weather_to_docx.document.localized_meteogram_document import (  # noqa: E402
+    ScientificDocumentGenerator,
+)
 
 __all__ = ["ScientificDocumentGenerator"]
